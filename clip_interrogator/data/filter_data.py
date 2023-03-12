e6f7ea9abe0bb474033ef2b9bb3978862f8d2c32
minarainbow <minarainbow@kaist.ac.kr>
2023-03-11 21:57:50 -0600

    updated data-lighting

--- clip_interrogator/data/filter_data.py
+++ clip_interrogator/data/filter_data.py
@@ -1,9 +1,9 @@
-lighting = ["light", "dark", "bright", "shadow"]
+lighting = ["light", "dark", "bright", "shadow", "glow"]
 camera_shot = ["angle", "shot", "view"]
 with open('flavors.txt') as source_file:
-    with open('camera_shot.txt', 'a') as dest_file:
+    with open('lighting.txt', 'a') as dest_file:
         for line in source_file:
-            for word in camera_shot:
-                if word in line:
+            for word in lighting:
+                if word in line and not "slight" in line :
                     dest_file.write(line)
     
